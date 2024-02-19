bc220ac0ca8a06c3a1b0b0d04725bfde6cbbee0a
Elizaveta Ustinova <weta.esu@yandex.ru>
2024-02-19 19:23:13 +0300

    Initial commit

---
+++ src/main/java/org/example/BonusMilesService.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int result;
+        int miles = (cost / 20);
+        {
+            result = miles;
+        }
+        return result;
+    }
+}
+
